d94c2bcf6df765242b62ba55d7f1c36a0fb7b7b7
Ibrahim-nassih <binni3673@gmail.com>
2024-03-13 18:13:36 -0700

    update

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Print a message indicating that C is fun
+console.log('C is fun');
+
+// Print a message indicating that Python is cool
+console.log('Python is cool');
+
+// Print a message indicating that JavaScript is amazing
+console.log('JavaScript is amazing');
+
+// Additional functionality could be added here, such as prompting user input or performing calculations
+
